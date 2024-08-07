a9aba1f00195feb9cd1637549632aefe547ff3b3
Craig Wigg <wiggman@hotmail.co.uk>
2024-08-07 20:11:29 +0100

    altered parameter name in playSound function

--- script.js
+++ script.js
@@ -25,6 +25,13 @@ tidus.onmouseleave = function () {
     tidus.style.backgroundImage = "url('./images/Tidus.jpg')";
 };
 
+function playSound (sound, path) {
+    let path = `./sounds/${path}/`;
+    const snd = new Audio(path + sound + ".mp3");
+    snd.currentTime = 0;
+    snd.play();
+}
+
 /*
 const audio = new Audio('./burp.mp3');
 
